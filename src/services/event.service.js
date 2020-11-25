788cb7ff84a136106aaab8370f10cae254c045bf
Kushniruk Taras <ktp30002@gmail.com>
2020-11-25 16:37:08 +0200

    Update event service

--- src/services/event.service.js
+++ src/services/event.service.js
@@ -1,17 +1,28 @@
-const { Event } = require('../models/event.model');
+const config = require('config');
+
 const { HttpError } = require('../errors');
+const { Event } = require('../models/event.model');
 
 async function addNewEvent({
   title, description, date, color,
 }, { userId }) {
-  try {
+
+  const { floodControl: { time: FLOOD_TIME }} = config;
+  const { floodControl: { count: ELEM_COUNT }} = config;
+
+  const userEvents = await Event.find({ userId,
+    createdAt: {
+      $gt: new Date(Date.now() - FLOOD_TIME)
+    }});
+
+  if (userEvents.length > ELEM_COUNT) {
+    throw new HttpError({
+      message: 'Stop flooding',
+      code: 401,
+    });
+  } else {
     await Event.create({
       userId, title, description, date, color,
-    });
-  } catch (error) {
-    throw new HttpError({
-      message: error,
-      code: 500,
     });
   }
 }
@@ -19,32 +30,18 @@ async function addNewEvent({
 async function editEvent({
   title, id, description, date, color,
 }) {
-  try {
-    await Event.updateOne({ _id: id }, {
-      $set: {
-        title,
-        description,
-        date,
-        color,
-      },
-    });
-  } catch (error) {
-    throw new HttpError({
-      message: error,
-      code: 500,
-    });
-  }
+  await Event.updateOne({ _id: id }, {
+    $set: {
+      title,
+      description,
+      date,
+      color,
+    },
+  });
 }
 
 async function deleteEvent({ id }) {
-  try {
-    await Event.deleteOne({ _id: id }).exec();
-  } catch (error) {
-    throw new HttpError({
-      message: 'Event not found',
-      code: 404,
-    });
-  }
+  await Event.deleteOne({ _id: id }).exec();
 }
 
 module.exports = {
